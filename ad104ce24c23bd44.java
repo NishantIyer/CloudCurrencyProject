fbfeedaa3510b3bd769b1da9aa0a956568978aed
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 15

---
+++ ad104ce24c23bd44.java
@@ -0,0 +1,11 @@
+// ad104ce24c23bd44.java
+public class ad104ce24c23bd44 {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
