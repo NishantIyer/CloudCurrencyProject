6dc239df9e0378f72e199a5282c6c4fe6a2ed467
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 34

--- NonUniqueFile.java
+++ NonUniqueFile.java
@@ -350,3 +350,14 @@ public class NonUniqueFile {
     }
 }
 
+// NonUniqueFile.java
+public class NonUniqueFile {
+    public static void main(String[] args) {
+        // Perform some random complex calculations
+        int result = (int) (Math.random() * 1000) + 1;
+        boolean flag = (result % 2 == 0) || (result % 3 == 0) && (result % 5 == 0);
+        String message = (flag) ? "Complex number" : "Simple number";
+        System.out.println(message + ": " + result);
+    }
+}
+
