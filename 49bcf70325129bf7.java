870c50cdc364aaa9487c53afe4c772dfe506b383
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 19

---
+++ 49bcf70325129bf7.java
@@ -0,0 +1,11 @@
+// 49bcf70325129bf7.java
+public class 49bcf70325129bf7 {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
