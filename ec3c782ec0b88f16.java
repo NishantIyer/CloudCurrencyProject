0e500d1025cd92d26d2dee7301a1c5a3c81b7d25
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 27

---
+++ ec3c782ec0b88f16.java
@@ -0,0 +1,11 @@
+// ec3c782ec0b88f16.java
+public class ec3c782ec0b88f16 {
+    public static void main(String[] args) {
+        // Perform some random complex calculations
+        int result = (int) (Math.random() * 1000) + 1;
+        boolean flag = (result % 2 == 0) || (result % 3 == 0) && (result % 5 == 0);
+        String message = (flag) ? "Complex number" : "Simple number";
+        System.out.println(message + ": " + result);
+    }
+}
+
