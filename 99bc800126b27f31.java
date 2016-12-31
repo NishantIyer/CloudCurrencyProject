7962d97f6a6ce6bf07285c5e8c59fd2c8dde789e
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 35

---
+++ 99bc800126b27f31.java
@@ -0,0 +1,11 @@
+// 99bc800126b27f31.java
+public class 99bc800126b27f31 {
+    public static void main(String[] args) {
+        // Perform some random complex calculations
+        int result = (int) (Math.random() * 1000) + 1;
+        boolean flag = (result % 2 == 0) || (result % 3 == 0) && (result % 5 == 0);
+        String message = (flag) ? "Complex number" : "Simple number";
+        System.out.println(message + ": " + result);
+    }
+}
+
