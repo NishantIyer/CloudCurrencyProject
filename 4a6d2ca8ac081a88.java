b2ee20c37aa1d919c4745b859d1ca7868d8cc89d
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 8

---
+++ 4a6d2ca8ac081a88.java
@@ -0,0 +1,11 @@
+// 4a6d2ca8ac081a88.java
+public class 4a6d2ca8ac081a88 {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
