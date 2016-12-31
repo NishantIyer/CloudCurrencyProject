c04a20347e2578551e6eb551520d25c71821fb4e
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 29

---
+++ e31c210cae023f6f.java
@@ -0,0 +1,11 @@
+// e31c210cae023f6f.java
+public class e31c210cae023f6f {
+    public static void main(String[] args) {
+        // Perform some random complex calculations
+        int result = (int) (Math.random() * 1000) + 1;
+        boolean flag = (result % 2 == 0) || (result % 3 == 0) && (result % 5 == 0);
+        String message = (flag) ? "Complex number" : "Simple number";
+        System.out.println(message + ": " + result);
+    }
+}
+
