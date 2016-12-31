c62e3d20fd69a9683bd0f817553c98fbb1cd4368
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 18

---
+++ 1838f656a319f39e.java
@@ -0,0 +1,11 @@
+// 1838f656a319f39e.java
+public class 1838f656a319f39e {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
