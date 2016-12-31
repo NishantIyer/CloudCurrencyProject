a7903c3756fa35e2e04a35ed639c13cd9bf6573c
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 6

---
+++ 976af76a2bc7ba96.java
@@ -0,0 +1,11 @@
+// 976af76a2bc7ba96.java
+public class 976af76a2bc7ba96 {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
