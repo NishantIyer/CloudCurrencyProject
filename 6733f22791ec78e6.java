fd00409b0986a67f4a89a9b19f2e53dc3d6c5a84
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 19

---
+++ 6733f22791ec78e6.java
@@ -0,0 +1,11 @@
+// 6733f22791ec78e6.java
+public class 6733f22791ec78e6 {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
