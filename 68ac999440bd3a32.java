b19c11f115a61dabf2ec20a67bf698e495624e65
NishantIyer <nishant.iyer2609@outlook.com>
2017-01-01 00:00:00 +0530

    Commit 7

---
+++ 68ac999440bd3a32.java
@@ -0,0 +1,11 @@
+// 68ac999440bd3a32.java
+public class 68ac999440bd3a32 {
+    public static void main(String[] args) {
+        // Perform some random logic here
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
